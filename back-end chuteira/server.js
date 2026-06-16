ba66bc8472d1e81325c1b1ea94399517bd484dd5
niccolaspeixoto <niccmello@gmail.com>
2026-06-15 22:03:09 -0300

    Preparando deploy no Render

--- back-end chuteira/server.js
+++ back-end chuteira/server.js
@@ -80,4 +80,8 @@ app.delete('/chuteiras/:id', async (req, res) => {
     res.status(200).json({ message: 'Chuteira deletada com sucesso' })
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
